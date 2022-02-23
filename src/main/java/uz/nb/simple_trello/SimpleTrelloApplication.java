09ceb0a0502b0cd5d9c9ad003feee9ece8d355cf
Botirov Najmiddin <95509783+NajmiddinBG@users.noreply.github.com>
2022-02-23 14:26:24 +0500

    initial commit

---
+++ src/main/java/uz/nb/simple_trello/SimpleTrelloApplication.java
@@ -0,0 +1,13 @@
+package uz.nb.simple_trello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTrelloApplication.class, args);
+    }
+
+}
